b27a088ae216f7126c5b8c71f44a9f25f49e0f98
Amber <amber20240@gmail.com>
2019-11-25 16:14:13 +0800

    add toDOList / analytics / ringTones  leftIcon a

---
+++ analytics.js
@@ -0,0 +1,15 @@
+var toDoListDiv=document.querySelector(".toDoList");
+function toDoListOpen(){
+    self.location.href="toDoList.html";
+};
+toDoListDiv.addEventListener("click",toDoListOpen,false);
+var analyticsDiv=document.querySelector(".analytics");
+function analyticsOpen(){
+    self.location.href="analytics.html";
+};
+analyticsDiv.addEventListener("click",analyticsOpen,false);
+var ringTonesDiv=document.querySelector(".ringTones");
+function ringTonesOpen(){
+    self.location.href="ringTones.html";
+};
+ringTonesDiv.addEventListener("click",ringTonesOpen,false);
--- toDoList.js
+++ toDoList.js
@@ -31,3 +31,18 @@ function addDone(e){
     list(data);
 };
 toDoListUl.addEventListener("click",addDone,false);
+var toDoListDiv=document.querySelector(".toDoList");
+function toDoListOpen(){
+    self.location.href="toDoList.html";
+};
+toDoListDiv.addEventListener("click",toDoListOpen,false);
+var analyticsDiv=document.querySelector(".analytics");
+function analyticsOpen(){
+    self.location.href="analytics.html";
+};
+analyticsDiv.addEventListener("click",analyticsOpen,false);
+var ringTonesDiv=document.querySelector(".ringTones");
+function ringTonesOpen(){
+    self.location.href="ringTones.html";
+};
+ringTonesDiv.addEventListener("click",ringTonesOpen,false);
